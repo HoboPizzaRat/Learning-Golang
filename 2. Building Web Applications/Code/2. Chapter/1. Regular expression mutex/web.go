01184024976f599dec268dc19e4f13f2b16b4487
HoboPizzaRat <jaloveikhilt@gmail.com>
2026-09-19 18:51:18 +0300

    Doing sutff
    stuff

---
+++ 2. Building Web Applications/Code/2. Chapter/1. Regular expression mutex/web.go
@@ -0,0 +1,29 @@
+package main
+
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+const (
+	PORT = ":8080"
+)
+
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageID := vars["id"]
+	fileName := "files/" + pageID + ".html"
+	http.ServeFile(w,r,fileName)
+}
+
+func main() {
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/pages/{id:[0-9]+}", pageHandler)
+	rtr.HandleFunc("/homepage", pageHandler)
+	rtr.HandleFunc("/contact", pageHandler)
+	http.Handle("/", rtr)
+	http.ListenAndServe(PORT, nil)
+}
